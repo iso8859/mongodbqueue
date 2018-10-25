fdb2f56ba17a53c97e4c59bcd567f10bda6c7fb8
Remi THOMAS <remi@xtware.com>
2018-10-25 14:41:21 +0200

    First commit

---
+++ MongoQueue2.cs
@@ -0,0 +1,92 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MongoHelper
+{
+    public class MongoQueue2
+    {
+        string m_szDatabase, m_szCollection;
+        IMongoDatabase m_db;
+        BsonValue m_lastId = BsonMinKey.Value;
+        bool m_capped;
+
+        // You can't delete record from a capped collection and you can change its size.
+        public MongoQueue2(string connectionString, string database, string collection, bool capped, long queueSize = 1024 * 1024)
+        {
+            m_szDatabase = database;
+            m_szCollection = collection;
+            m_capped = capped;
+
+            m_db = new MongoClient(connectionString).GetDatabase(database);
+            if (capped)
+            {
+                var list = m_db.ListCollections(new ListCollectionsOptions() { Filter = Builders<BsonDocument>.Filter.Eq("name", collection) }).ToList();
+                if (list.Count == 0)
+                {
+                    try
+                    {
+                        var options = new CreateCollectionOptions { Capped = true, MaxSize = queueSize };
+                        m_db.CreateCollection(collection, options);
+                    }
+                    catch
+                    {
+                        // assume that any exceptions are because the collection already exists ...
+                    }
+                }
+            }
+            GetLastId();
+        }
+
+        public void SetLastId<T>(object o)
+        {
+            if (o != null)
+            {
+                // Retreive the id
+                var idProvider = MongoDB.Bson.Serialization.BsonSerializer.LookupSerializer(typeof(T)) as MongoDB.Bson.Serialization.IBsonIdProvider;
+                object id;
+                Type nominal;
+                MongoDB.Bson.Serialization.IIdGenerator generator;
+                if (idProvider.GetDocumentId(o, out id, out nominal, out generator))
+                    m_lastId = BsonValue.Create(id);
+            }
+        }
+
+        public void GetLastId()
+        {
+            SetLastId<BsonDocument>(m_db.GetCollection<BsonDocument>(m_szCollection).Find(bson => true).SortByDescending(bson => bson["_id"]).FirstOrDefault());
+        }
+
+        public async Task SendAsync<T>(T doc)
+        {
+            await m_db.GetCollection<T>(m_szCollection).InsertOneAsync(doc);
+        }
+
+        // The callback should return the id for this record
+        public async Task ReceiveAsync<T>(Func<T, BsonValue> callback, FilterDefinition<T> query = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            while (true)
+            {
+                FilterDefinition<T> query2 = Builders<T>.Filter.Gt("_id", m_lastId); // We search event we could have miss since last call
+                if (query != null)
+                    query2 = Builders<T>.Filter.And(query, query2);
+
+                var options = new FindOptions<T> { CursorType = CursorType.TailableAwait };
+                using (var cursor = await m_db.GetCollection<T>(m_szCollection).FindAsync(query2, options, cancellationToken))
+                {
+                    await cursor.ForEachAsync(document =>
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        m_lastId = callback(document);
+                    }, cancellationToken); // <= This cancellationToken is the important one to allow clean exit
+                }
+                // the tailable cursor died so loop through and restart it
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageQueue
+{
+    public class Event
+    {
+        public ObjectId _id;
+        public BsonValue taskId;
+        public string source;
+        public string filter;
+        public DateTime dt;
+    }
+
+    class Program
+    {
+        // Example message queue with mongodb. Small multi process chat
+        // My advise is to use an event collection only to trigger the event
+        // and use another collection with all the infos about the event (message info...)
+        // because capped collection have some disavantages (can't delete)
+        static void Main(string[] args)
+        {
+            string cnx = "mongodb://127.0.0.1";
+            CancellationTokenSource exitToken = new CancellationTokenSource();
+
+            var db = new MongoDB.Driver.MongoClient(cnx).GetDatabase("devapps");
+            MongoHelper.MongoQueue2 mq = new MongoHelper.MongoQueue2(cnx, "devapps", "mq", true);
+            Task t1 = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    // Uncomment to simulate slow treatment
+                    // await System.Threading.Tasks.Task.Delay(1000, source.Token);
+                    await mq.ReceiveAsync<Event>((Event e) =>
+                    {
+                        Console.WriteLine($"{e.dt.ToLocalTime()}\t{e.source}\t{e.filter}");
+                        return e._id;
+                    },
+                    Builders<Event>.Filter.Eq(x => x.filter, "mq"),
+                    exitToken.Token);
+                }
+            }, exitToken.Token);
+
+            string line = "";
+            while ((line=Console.ReadLine())!="")
+            {
+                Task.Run(async () => await mq.SendAsync<Event>(new Event() { source = line, dt = DateTime.Now, filter = "mq" }));
+            }
+            exitToken.Cancel();
+            Task.WaitAny(t1);
+        }
+    }
+}
